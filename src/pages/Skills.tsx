01079b440c1478436df45991ab4d8c1ac803d40c
sandakirani <vihangisandakirani@gmail.com>
2025-08-18 16:30:05 +0530

    reduce model size

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -58,45 +58,45 @@ export default function Skills() {
           <Suspense fallback={null}>
             {category === "frontend" && (
               <>
-                <Model path="/model/react.glb" position={[-8, 0, 0]} url="https://react.dev/" />
-                <Model path="/model/html.glb" position={[-4, 0, 0]} url="https://www.w3schools.com/html/" />
-                <Model path="/model/css.glb" position={[0, 0, 0]} url="https://www.w3schools.com/css/" />
-                <Model path="/model/js.glb" position={[4, 0, 0]} url="https://www.w3schools.com/js/" />
-                <Model path="/model/expo.glb" position={[8, 0, 0]} url="https://expo.dev/" />
+                <Model path="/model/react-draco.glb" position={[-8, 0, 0]} url="https://react.dev/" />
+                <Model path="/model/html-draco.glb" position={[-4, 0, 0]} url="https://www.w3schools.com/html/" />
+                <Model path="/model/css-draco.glb" position={[0, 0, 0]} url="https://www.w3schools.com/css/" />
+                <Model path="/model/js-draco.glb" position={[4, 0, 0]} url="https://www.w3schools.com/js/" />
+                <Model path="/model/expo-draco.glb" position={[8, 0, 0]} url="https://expo.dev/" />
               </>
             )}
 
             {category === "backend" && (
               <>
-                <Model path="/model/csharp.glb" position={[-4, 0, 0]} url="https://learn.microsoft.com/en-us/dotnet/csharp/" />
-                <Model path="/model/php.glb" position={[0, 0, 0]} url="https://www.php.net/" />
-                <Model path="/model/kotlin.glb" position={[4, 0, 0]} url="https://kotlinlang.org/" />
+                <Model path="/model/csharp-draco.glb" position={[-4, 0, 0]} url="https://learn.microsoft.com/en-us/dotnet/csharp/" />
+                <Model path="/model/php-draco.glb" position={[0, 0, 0]} url="https://www.php.net/" />
+                <Model path="/model/kotlin-draco.glb" position={[4, 0, 0]} url="https://kotlinlang.org/" />
               </>
             )}
 
             {category === "database" && (
               <>
-                <Model path="/model/mongodb.glb" position={[-2, 0, 0]} url="https://www.mongodb.com/" />
-                <Model path="/model/sQlite.glb" position={[2, 0, 0]} url="https://www.sqlite.org/" />
+                <Model path="/model/mongodb-draco.glb" position={[-2, 0, 0]} url="https://www.mongodb.com/" />
+                <Model path="/model/sQlite-draco.glb" position={[2, 0, 0]} url="https://www.sqlite.org/" />
               </>
             )}
 
             {category === "tools" && (
               <>
-                <Model path="/model/android.glb" position={[-10, 0, 0]} url="https://developer.android.com/studio" />
-                <Model path="/model/vscode.glb" position={[-6, 0, 0]} url="https://code.visualstudio.com/" />
-                <Model path="/model/vs.glb" position={[-2, 0, 0]} url="https://visualstudio.microsoft.com/" />
-                <Model path="/model/postman.glb" position={[2, 0, 0]} url="https://www.postman.com/" />
-                <Model path="/model/matlab.glb" position={[6, 0, 0]} url="https://www.mathworks.com/products/matlab.html" />
-                <Model path="/model/blender.glb" position={[10, 0, 0]} url="https://www.blender.org/" />
-                <Model path="/model/figma.glb" position={[-2, -4, 0]} url="https://www.figma.com/" />
-                <Model path="/model/canva.glb" position={[2, -4, 0]} url="https://www.canva.com/" />
+                <Model path="/model/android-draco.glb" position={[-10, 0, 0]} url="https://developer.android.com/studio" />
+                <Model path="/model/vscode-draco.glb" position={[-6, 0, 0]} url="https://code.visualstudio.com/" />
+                <Model path="/model/vs-draco.glb" position={[-2, 0, 0]} url="https://visualstudio.microsoft.com/" />
+                <Model path="/model/postman-draco.glb" position={[2, 0, 0]} url="https://www.postman.com/" />
+                <Model path="/model/matlab-draco.glb" position={[6, 0, 0]} url="https://www.mathworks.com/products/matlab.html" />
+                <Model path="/model/blender-draco.glb" position={[10, 0, 0]} url="https://www.blender.org/" />
+                <Model path="/model/figma-draco.glb" position={[-2, -4, 0]} url="https://www.figma.com/" />
+                <Model path="/model/canva-draco.glb" position={[2, -4, 0]} url="https://www.canva.com/" />
               </>
             )}
 
              {category === "version" && (
               <>
-                <Model path="/model/git.glb" position={[0, 0, 0]} url="https://github.com" />
+                <Model path="/model/git-draco.glb" position={[0, 0, 0]} url="https://github.com" />
               </>
             )}
           </Suspense>
